030deb82db442896e7c0ef80bc59c260c5bafc12
vlad3005 <vladborunov396@gmail.com>
2026-02-27 17:58:41 +0300

    first version

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,45 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value <= 1) return false;
+  if (value == 2 || value == 3) return true;
+  if (value % 2 == 0 || value % 3 == 0) return false;
+  for (uint64_t i = 5; i * i <= value; i += 6) {
+    if (value % i == 0 || value % (i + 2) == 0) return false;
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n == 0) return 0;
+  uint64_t count = 0;
+  uint64_t num = 0;
+  while (count < n) {
+    num++;
+    if (checkPrime(num)) {
+      count++;
+    }
+  }
+  return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  if (value < 2) return 2;
+  uint64_t next = value + 1;
+  if (next % 2 == 0) next++; 
+  while (!checkPrime(next)) {
+    next += 2;
+  }
+  return next;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  if (hbound <= 2) return 0;
+  uint64_t sum = 2;
+  for (uint64_t i = 3; i < hbound; i += 2) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -5,13 +5,66 @@
 #include "alg.h"
 
 
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(st1, checkPrime1) {
+  EXPECT_TRUE(checkPrime(2));
 }
+
+TEST(st1, checkPrime2) {
+  EXPECT_FALSE(checkPrime(0));
+}
+
+TEST(st1, checkPrime3) {
+  EXPECT_FALSE(checkPrime(1));
+}
+
+TEST(st1, checkPrime4) {
+  EXPECT_TRUE(checkPrime(13));
+}
+
+TEST(st1, checkPrime5) {
+  EXPECT_FALSE(checkPrime(15));
+}
+
+TEST(st1, nPrime1) {
+  EXPECT_EQ(nPrime(1), 2);
+}
+
+TEST(st1, nPrime2) {
+  EXPECT_EQ(nPrime(2), 3);
+}
+
+TEST(st1, nPrime3) {
+  EXPECT_EQ(nPrime(5), 11);
+}
+
+TEST(st1, nPrime4) {
+  EXPECT_EQ(nPrime(10), 29);
+}
+
+TEST(st1, nextPrime1) {
+  EXPECT_EQ(nextPrime(2), 3);
+}
+
+TEST(st1, nextPrime2) {
+  EXPECT_EQ(nextPrime(4), 5);
+}
+
+TEST(st1, nextPrime3) {
+  EXPECT_EQ(nextPrime(17), 19);
+}
+
+TEST(st1, sumPrime1) {
+  EXPECT_EQ(sumPrime(2), 0);
+}
+
 TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+  EXPECT_EQ(sumPrime(3), 2);
+}
+
+TEST(st1, sumPrime3) {
+  EXPECT_EQ(sumPrime(5), 5);
+}
+
+TEST(st1, sumPrime4) {
+  EXPECT_EQ(sumPrime(10), 17);
 }
